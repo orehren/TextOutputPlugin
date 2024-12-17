845dfe3c600e773b4b38572c2b60b7e138e7d95d
orehren <o.rehren@gmail.com>
2024-12-17 07:04:35 +0100

    Initialize Plugin

--- actions/mappings.py
+++ actions/mappings.py
@@ -30,26 +30,31 @@ class KeyMapper:
             self.xkb_state = None
             
     def map_char(self, char: str) -> List[int]:
-      if not self.xkb_state or not self.xkb_keymap:
-          log.error("xkbcommon is not set up correctly.")
-          return []
+        if not self.xkb_state or not self.xkb_keymap:
+             log.error("xkbcommon is not set up correctly.")
+             return []
 
-      keycodes = []
-      utf32_char = ord(char)
+        log.debug(f"Keymap Attributes: {dir(self.xkb_keymap)}")
 
-      found_keycodes = []
-      for keycode in self.xkb_keymap:
-          symbols = self.xkb_keymap.key_get_syms_by_level(keycode, 0, 0)
-          if not symbols:
-              continue
-              
-      log.debug(f"Symbol Attributes: {dir(symbols)}") 
-          for symbol in symbols:
-            if xkb.keysym_to_string(symbol) == utf32_char:
-               found_keycodes.append(keycode)
-               break
-      if not found_keycodes:
-          log.warning(f"No keycode found for character: {char} (UTF-32: {utf32_char})")
-      else:
-          keycodes.extend(found_keycodes)
-      return keycodes
+        keycodes = []
+        utf32_char = ord(char)
+        
+        found_keycodes = []
+        for keycode in self.xkb_keymap:
+             symbols = self.xkb_keymap.key_get_syms_by_level(keycode, 0, 0)
+             if not symbols:
+                 continue
+             
+             log.debug(f"Symbol Attributes: {dir(symbols)}")
+             for symbol in symbols:
+                log.debug(f"Symbol: {symbol}, Utf-32: {xkb.keysym_to_string(symbol)}")
+                if xkb.keysym_to_string(symbol) == char:
+                    found_keycodes.append(keycode)
+                    break
+                    
+        if not found_keycodes:
+            log.warning(f"No keycode found for character: {char} (UTF-32: {utf32_char})")
+        
+        keycodes.extend(found_keycodes)
+
+        return keycodes
